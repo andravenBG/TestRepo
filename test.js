b0c25abf80c0f1b2c5156273ce2e66b8566ffb8d
andravenBG <andrey.ivanov@infonetica.net>
2024-01-12 17:33:47 +0200

    commit F

--- test.js
+++ test.js
@@ -5,3 +5,5 @@ console.log("First commit");
 console.log("This commit was made on the main branch");
 
 console.log("Yest another commit made on the made branch!");
+
+console.log("This commit was acctually done on branch-A");
